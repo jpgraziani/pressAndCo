dff4641861ce74e8136f919a422a5a923967d3ab
Jonathan Graziani <jpgraziani@gmail.com>
2020-11-20 22:41:53 -0700

    started pseudo outline app

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
 'use strict';
 
 const API_KEY = config.API_KEY;
+
+/*****************************************/
+/* NEWYORK TIMES API URLS */
+/*****************************************/
+
+const newsWireURL = 'https://api.nytimes.com/svc/news/v3/content/all/';
+
+const searchURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+
+
+/*****************************************/
+/* GLOBAL PARAMS */
+/*****************************************/
+
+
+
+
+
+/*****************************************/
+/* NEWSWIRE API DATA */
+/*****************************************/
+
+
+
+
+
+/*****************************************/
+/* SEARCH ARTICLES API */
+/*****************************************/
+
+
+
+
+
+/*****************************************/
+/* EVENT HANDLES */
+/*****************************************/
+//handle for deep search
+function handleSearchArticlesBtn() {
+
+}
+
+//handle for sections
+function handleSectionsSubBtn() {
+
+}
+
+//handle clear search results
+function handleClearSearch() {
+  
+}
+
+/*****************************************/
+/* LOAD THESE ITEMS WHEN APP PAGE LOADS */
+/*****************************************/
+//CREATE FUNCTION THAT HOLDS ALL LOAD FUNCTIONS
